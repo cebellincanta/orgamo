2cc5356ac2977912c3131b2bcc5120ab039af9cd
carlos <cebellincanta@gmail.com>
2023-06-05 14:29:24 -0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Banner from './components/Banner';
+import Formulario from './components/Formulario';
+import Time from './components/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'C#',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Java',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Swift',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'GO',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    }
+  ]
+
+  const [programdores, setProgramadores] = useState([])
+  const aoNovoProgramadorAdicionado = (programador) => {
+    console.log(programador)
+    setProgramadores([...programdores, programador])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoProgramadorCadastrado={programador => aoNovoProgramadorAdicionado(programador)}/>
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Formulario/index.js
@@ -0,0 +1,72 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+import { useState } from 'react'
+
+const Formulario = (props) => {
+    
+    const [nome, setNome] = useState('')
+    const [sobrenome, setSobrenome] = useState('')
+    const [telefone, setTelefone] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [linguagem, setLinguagem] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoProgramadorCadastrado({
+            nome: nome, 
+            sobrenome: sobrenome, 
+            telefone: telefone, 
+            cpf: cpf, 
+            linguagem: linguagem
+        })
+    }
+    return (
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>  
+                <h2>Preencha dados basicos para se inscrever no site</h2>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Sobrenome"  
+                    placeholder="Digite seu sobrenome"
+                    valor={sobrenome}
+                    aoAlterado={valor => setSobrenome(valor)}
+                    />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Telefone" 
+                    placeholder="Digite seu telefone"
+                    valor={telefone}
+                    aoAlterado={valor => setTelefone(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="CPF" 
+                    placeholder="Digite o CPF"
+                    valor={cpf}
+                    aoAlterado={valor => setCpf(valor)}
+                />
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Linguagem" 
+                    itens={props.times} 
+                    valor={linguagem}
+                    aoAlterado={valor => setLinguagem(valor)}
+                    />
+                <Botao>
+                    Adicionar ao time
+                </Botao> 
+            </form>
+        </section>
+    )
+}
+
+export default Formulario   
